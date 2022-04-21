cc8895c2c571fe5a4ef098a430941095da6eefbc
kbenn5 <kbenneticha@gmail.com>
2022-04-21 15:38:43 -0400

    finished leds.c

--- src/leds.c
+++ src/leds.c
@@ -11,7 +11,8 @@
 void leds__init() {
 #if LEDS == ON
     // setting LEDs as output pins
-    DDRC = 0xFF;
+    DDRC = 0x03; //makes first and second pins of PORTC outputs
+
     //pinMode(RED, OUTPUT);
     //pinMode(GREEN, OUTPUT);
     leds_off();
@@ -20,28 +21,33 @@ void leds__init() {
 void leds__off() {
 #if LEDS == ON
     // setting LEDs as low (turning them off)
-    analogWrite(RED, 0);
-    analogWrite(GREEN, 0);
+    PORTC &= ~0x03; //turn off both LEDs
+    //analogWrite(RED, 0);
+    //analogWrite(GREEN, 0);
 #endif
 }
 void leds__green() {
 #if LEDS == ON
     // only turn on green LED
-    analogWrite(RED, 0);
-    analogWrite(GREEN, 255);
+    //analogWrite(RED, 0);
+    //analogWrite(GREEN, 255);
+    PORTC |= (0x02); //turns on green LED
+
 #endif
 }
 void leds__red() {
 #if LEDS == ON
     // only turn on red LED
-    analogWrite(RED, 255);
-    analogWrite(GREEN, 0);
+    //analogWrite(RED, 255);
+    //analogWrite(GREEN, 0);
+    PORTC |= (0x01); //turns on red LED
 #endif
 }
 void leds__both() {
 #if LEDS == ON
     // turn on both LEDs
-    analogWrite(RED, 255);
-    analogWrite(GREEN, 255);
+    //analogWrite(RED, 255);
+    //analogWrite(GREEN, 255);
+    PORTC |= 0x03; //turn on both LEDs
 #endif
 }
